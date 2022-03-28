5c3046cc6ba55aa21f77ad18b978b6b7959b3bc0
dcts <thomas@liist.com>
2022-03-28 12:45:00 +0200

    :sparkle: add boilerplate component

---
+++ src/components/helper/BoilerplateComponent.js
@@ -0,0 +1,10 @@
+import { useRef, useEffect, useState } from "react";
+
+const BoilerplateComponent = () => { // <= CHANGE THIS
+  return (
+    <div className="boilerplate-component-container"> {/* <= CHANGE THIS */}
+    </div>
+  );
+};
+
+export default BoilerplateComponent; // <= CHANGE THIS
