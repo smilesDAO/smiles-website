4c139c877639ab87ddd028981df67614cd9af5ae
dcts <thomas@liist.com>
2022-03-29 16:08:27 +0200

    :sparkle: hardcode ticker component - move all styling inside CSS

--- src/components/home/_Index.js
+++ src/components/home/_Index.js
@@ -5,29 +5,14 @@ import Wtf from './Wtf';
 import Tutorial from './Tutorial';
 import Jobs from './Jobs';
 import Faq from './Faq';
-import SmilesColors from '../atoms/SmilesColors';
 
 const Home = () => {
   return (
     <div className="home-container">
       <Banner/>
-      <Ticker
-        speed={10}
-        backgroundColor={SmilesColors.green}
-        height={70}
-        imageUrl={"/tickerImages/tickerPoweredBy.svg"}
-        scale={0.8}
-        imageWidth={2939}
-        // paddingY={"20px"}
-        />
+      <Ticker tickerContent={"powered-by"} />
       <Wtf/>
-      {/* <Ticker
-        speed={1}
-        backgroundColor={SmilesColors.green}
-        height={140}
-        // scale={0.2}
-        imageUrl={"/tickerImages/tickerHowItWorks.svg"}
-      /> */}
+      <Ticker tickerContent={"how-it-works"} />
       <Tutorial/>
       <Jobs/>
       <Faq/>
